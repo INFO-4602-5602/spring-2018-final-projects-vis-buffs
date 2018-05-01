75d2eb235c376ca85a90a4cb44f2d056d0408afd
yawenz <yawen.zhang@colorado.edu>
2018-04-30 22:52:55 -0600

    fix vis_3 position problem

--- Submission/py/features.py
+++ Submission/py/features.py
@@ -7,7 +7,7 @@ import numpy as np
 if __name__ == "__main__":
 
     cat_data = pd.read_csv("../data/review_cat_user_userFeature_100user.csv")
-    loc_data = pd.read_csv("../data/buiness_loc_star50000.csv")
+    loc_data = pd.read_csv("../data/business_loc_star_US.csv")
     cat_data_sel = pd.read_csv("../data/restaurant_category_rating_select.csv")
     # print(review_data)
 
@@ -19,8 +19,8 @@ if __name__ == "__main__":
 
     cat_data_sel['avg'] =  cat_data_sel["1"]*0.01 + cat_data_sel["2"]*0.02 + cat_data_sel["3"]*0.03 + cat_data_sel["4"]*0.04 + cat_data_sel["5"]*0.05
     cat_data_sel = cat_data_sel[['category', 'avg']]
-    cat_data_sel.to_csv("../data/restaurant_category_rating_sel_avg.csv")
+    # cat_data_sel.to_csv("../data/restaurant_category_rating_sel_avg.csv")
 
     # restaurant loc & rating
     loc_vs_rating = loc_data.groupby("state")['stars'].mean()
-    #print(loc_vs_rating)
+    print(loc_vs_rating)
